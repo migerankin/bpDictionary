9da74daa61ff0ecdae233a549ac57c0dc07f0921
migerankin <116479175+migerankin@users.noreply.github.com>
2023-07-20 18:33:14 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -97,7 +97,7 @@ var ctx = c.getContext('2d')
         futureDate = new Date(futureTimeStamp);
         futureDateelse = new Date(futureTimeStampelse);
 
-        console.log(futureDate);
+        // console.log(futureDate);
 
         document.getElementById('nowTime').innerHTML = `北京时间<br><b style="color: #edf5f5;">${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}</b>`
         if(dayOrNight==1){
