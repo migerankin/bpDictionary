54a7a3837d4a552abe2c6b27e91468172555b87c
migerankin <244283586@qq.com>
2023-07-22 13:34:27 +0800

    Update catchWord.js

--- js/catchWord.js
+++ js/catchWord.js
@@ -9,17 +9,32 @@ document.getElementsByClassName('catchWord_btn')[0].onclick = function(){
     uploadInput.addEventListener('change', (event) => {
       const file = event.target.files[0];
       demo2(file);
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        $('.IMGshowWindow').attr('src',e.target.result)
+      };
+      reader.readAsDataURL(file);
+      $('.IMGshowWindow').css('height','50px')
+      $('.IMGshowWindow').css('display','block')
+      $(".IMGshowWindow").on("wheel", function(event) {
+        event.preventDefault();
+        var delta = event.originalEvent.deltaY;
+        var scale = delta > 0 ? 0.9 : 1.4;
+        $(this).css("transform", "translateX(-50%) translateY(-50%) scale(" + scale + ")");
+      });
+      $('.IMGshowWindow').click(function(){
+        $('.IMGshowWindow').css('display','none')
+        isshowWindowClose=0
+    })
     });
     document.addEventListener('paste', handlePaste);
   }else{
     document.getElementsByClassName('catchWord_Window')[0].style.top = '-200px'
-    document.getElementById('catchTextBox').innerText = '选择图片文件或复制图片后ctrl+ v。受识别准确率影响，建议只截取两三个连贯的正确假名进行模糊搜索。'
+    // document.getElementById('catchTextBox').innerText = '选择图片文件或复制图片后ctrl+ v。受识别准确率影响，建议只截取两三个连贯的正确假名进行模糊搜索。'
     catchWord_WindowTimes=0
     document.removeEventListener('paste', handlePaste);
   }
 }
-// 获取上传的图片文件
-
 function handlePaste(event) {
   var items = event.clipboardData.items;
   for (var i = 0; i < items.length; i++) {
@@ -46,7 +61,7 @@ async function demo2(file) {
       await worker.initialize('jpn'); // 使用的语言
       const { data: { text } } = await worker.recognize(file);
       // document.getElementById('log').innerHTML = text;
-      document.getElementById('catchTextBox').innerText = text.replace(/\s/g, '')
+      document.getElementById('catchTextBox').innerText = text.replace(/\s/g, '')+'   [页面中部为原图用于核对字符，单击原图消失]'
       await worker.terminate();
   }
 }
