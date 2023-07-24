c087f11a42c09775b82aa7b0635fad44982fce17
migerankin <244283586@qq.com>
2023-07-24 10:37:14 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -2,7 +2,7 @@ var currentTimeStamp = Date.now();
 
 // 指定目标时间的时间戳（2023年7月19日17点22分10秒）
 var targetTimeStamp = new Date();
-targetTimeStamp.setHours(4);
+targetTimeStamp.setHours(23);
 targetTimeStamp.setMinutes(2);
 targetTimeStamp.setSeconds(10);
 if (currentTimeStamp < targetTimeStamp) {
@@ -85,7 +85,7 @@ var ctx = c.getContext('2d')
         now = new Date()
         currentTimeStamp = Date.now();
         targetTimeStamp = new Date();
-        targetTimeStamp.setHours(4);
+        targetTimeStamp.setHours(23);
         targetTimeStamp.setMinutes(2);
         targetTimeStamp.setSeconds(10);
         if (currentTimeStamp < targetTimeStamp) {
